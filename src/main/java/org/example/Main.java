fdc434d7d6a688ae4389c44e30a10a8ae4ae6e1d
Aidennnn97 <aidennnn97@gmail.com>
2023-03-02 09:33:31 +0900

    3단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = 1;
         System.out.println("== 명언 앱 ==");
         boolean run = true;
         while(run){
@@ -19,6 +20,7 @@ public class Main {
                     String quote = sc.nextLine();
                     System.out.printf("작가 : ");
                     String writer = sc.nextLine();
+                    System.out.println(num + "번 명언이 등록되었습니다.");
                     break;
             }
         }
