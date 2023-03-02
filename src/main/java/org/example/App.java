75ce8c7e54aa639c7daeb7499d189ded278d698a
Aidennnn97 <aidennnn97@gmail.com>
2023-03-02 17:34:36 +0900

    Request클래스 도입 후 명령어 해석 담당

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -29,26 +29,14 @@ public class App {
                 case "목록":
                     quoteController.list();
                     break;
-                case "삭제?id=1&writer=22":
-//                    Request request = new Request(command);
-//                    quoteController.delete();
-                    String[] commandBits = command.split("\\?", 2); // ?기준으로 쵀대 2개까지 나눈다 => 삭제?id=1&writer=22 -> [삭제, id=1&writer=22]
-                    String actionCode = commandBits[0]; // 명령 키워드(commandBits[0]) actionCode에 저장
+                case "삭제?id=1&writer=22&sentence=33":
+                    Request request = new Request(command);
+                    quoteController.delete();
 
-                    Map<String, String> params = new HashMap<>();   // id=1&writer=22 를 key, value 타입의 Map으로 저장할 공간 {"id":"1", "writer":"22"} 처럼
-
-                    String[] paramBits = commandBits[1].split("&");  // & 기준으로 나눔 => [id=1, writer=22]
-                    for(String paramStr : paramBits){
-                        String[] paramStrBits = paramStr.split("=", 2); // & 기준으로 나눈 id=1 과 writer=22를 다시 = 기준으로 나눈다 => [id, 1] [writer, 22]
-
-                        String key = paramStrBits[0];   // [id, 1] 의 id, [writer, 22]의 writer
-                        String value = paramStrBits[1]; // [id, 1] 의 1, [writer, 22]의 22
-
-                        params.put(key, value); // key와 value를 Map타입의 params에 저장
-                    }
-
-                    System.out.printf("actionCode: %s\n", actionCode);
-                    System.out.printf("params: %s\n", params);
+                    System.out.printf("actionCode: %s\n", request.getActionCode());
+                    System.out.printf("params.id: %s\n", request.getParam("id"));
+                    System.out.printf("params.writer: %s\n", request.getParam("writer"));
+                    System.out.printf("params.sentence: %s\n", request.getParam("sentence"));
 
                     break;
             }
--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Request {
-    public Request(String command) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Request {
+    private String actionCode;
+    private Map<String, String> params;
+    public Request(String command) {
+        String[] commandBits = command.split("\\?", 2); // ?기준으로 쵀대 2개까지 나눈다 => 삭제?id=1&writer=22 -> [삭제, id=1&writer=22]
+
+        actionCode = commandBits[0]; // 명령 키워드(commandBits[0]) actionCode에 저장
+
+        params = new HashMap<>();   // id=1&writer=22 를 key, value 타입의 Map으로 저장할 공간 {"id":"1", "writer":"22"} 처럼
+
+        String[] paramBits = commandBits[1].split("&");  // & 기준으로 나눔 => [id=1, writer=22]
+
+        for(String paramStr : paramBits){
+            String[] paramStrBits = paramStr.split("=", 3); // & 기준으로 나눈 id=1 과 writer=22를 다시 = 기준으로 나눈다 => [id, 1] [writer, 22]
+
+            String key = paramStrBits[0];   // [id, 1] 의 id, [writer, 22]의 writer
+            String value = paramStrBits[1]; // [id, 1] 의 1, [writer, 22]의 22
+
+            params.put(key, value); // key와 value를 Map타입의 params에 저장
+        }
     }
+
+    public String getParam(String key){
+        return params.get(key);
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
 }
