9f2f1fc2c73ae0b8cf13b552438366d61158c94c
Aidennnn97 <aidennnn97@gmail.com>
2023-03-02 09:28:40 +0900

    2단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        boolean run = true;
+        while(run){
+            System.out.printf("명령) ");
+            String order = sc.nextLine();
+            switch (order){
+                case "종료":
+                    run = false;
+                    break;
+                case "등록" :
+                    System.out.printf("명언 : ");
+                    String quote = sc.nextLine();
+                    System.out.printf("작가 : ");
+                    String writer = sc.nextLine();
+                    break;
+            }
+        }
     }
 }
